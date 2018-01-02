7b7d7fe853067e5d1362aac07203612c93b04caa
Alexander Friedland <friedlandaj@gmail.com>
2018-01-02 22:30:24 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,39 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(name: params["figure"]["name"])
+    if !Title.find_by_id(params["figure"]["title_ids"])
+      @figure.titles << Title.create(name: params["title"]["name"])
+    else
+      @figure.titles << Title.find_by_id(params["figure"]["title_ids"])
+    end
+
+    if !Landmark.find_by_id(params["figure"]["landmark_ids"])
+      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+    else
+      @figure.landmarks << Landmark.find_by_id(params["figure"]["landmark_ids"])
+    end
+
+    @figure.save
+    redirect to '/figures/:id'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
 end
