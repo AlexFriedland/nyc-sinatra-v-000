1ae2ed373aa467d0395a0e6f5eb1daa89d12e465
Alexander Friedland <friedlandaj@gmail.com>
2018-01-03 20:33:13 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,11 +1,21 @@
 class FiguresController < ApplicationController
 
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
   get '/figures' do
     erb :'/figures/index'
   end
 
-  get '/figures/new' do
-    erb :'/figures/new'
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
   end
 
   post '/figures' do
@@ -26,14 +36,8 @@ class FiguresController < ApplicationController
     redirect to '/figures/:id'
   end
 
-  get '/figures/:id/edit' do
-    @figure = Figure.find(params[:id])
-    erb :'/figures/edit'
-  end
-
-  get '/figures/:id' do
-    @figure = Figure.find(params[:id])
-    erb :'/figures/show'
+  post '/figures/:id' do
+    
   end
 
 end
